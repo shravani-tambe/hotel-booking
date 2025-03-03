a0bb6265c886d39c8c008695cf3424866692e2a7
Shravani Tambe <shravani.tambe@gmail.com>
2025-03-03 20:39:04 +0530

    Test commit

---
+++ frontend/src/redux/features/blogs/singleBlog/RelatedBlogs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RelatedBlogs = () => {
+  return (
+    <div>
+      <h3 className="text-2xl font-medium pt-8 px-8 pb-5">Related Blogs</h3>
+    </div>
+  );
+};
+
+export default RelatedBlogs;
--- frontend/src/redux/features/blogs/singleBlog/SingleBlog.jsx
+++ frontend/src/redux/features/blogs/singleBlog/SingleBlog.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useFetchBlogByIdQuery } from "../blogsApi";
 import SingleBlogCard from "./SingleBlogCard";
 import CommentCard from "../comments/CommentCard";
+import RelatedBlogs from "./RelatedBlogs";
 
 const SingleBlog = () => {
   const { id } = useParams();
@@ -19,7 +20,10 @@ const SingleBlog = () => {
               <SingleBlogCard blog={blog.post} />
               <CommentCard comments={blog.comments} />
             </div>
-            <div className="bg-white lg:w-1/3 w-full">Related Blogs</div>
+            <div className="bg-white lg:w-1/3 w-full">
+              {" "}
+              <RelatedBlogs />{" "}
+            </div>
           </div>
         )}
       </div>
